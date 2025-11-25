f98579a42a839b47c63c60b69aa38d85b3803961
PrinceEkine <princedagogoekine@gmail.com>
2025-11-25 12:06:37 +0100

    New Array

--- SingleDimensionalArray.java
+++ SingleDimensionalArray.java
@@ -3,6 +3,8 @@ public class SingleDimensionalArray{
 		String[] names = {"Ben","Daniel","Jack","Henry","Joy"};
 		int[] ages = {12,16,30,19,25};
 		
+		
+		
 		for(int i = 0; i < 5; i++){
 			System.out.printf("%s you are %d years old%n",names[i],ages[i]);
 		}
